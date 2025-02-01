2538b19ee26da91b1c5325dc872ca5f11d2a03fb
Данил Высоченко <vysochenkodanil@gmail.com>
2025-02-01 14:45:53 +0300

    привел еще ра к pep8

--- ya_note/notes/migrations/0001_initial.py
+++ ya_note/notes/migrations/0001_initial.py
@@ -17,11 +17,33 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Note',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(default='Название заметки', help_text='Дайте короткое название заметке', max_length=100, verbose_name='Заголовок')),
-                ('text', models.TextField(help_text='Добавьте подробностей', verbose_name='Текст')),
-                ('slug', models.SlugField(blank=True, help_text='Укажите адрес для страницы заметки. Используйте только латиницу, цифры, дефисы и знаки подчёркивания', max_length=100, unique=True, verbose_name='Адрес для страницы с заметкой')),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('id',
+                 models.BigAutoField(
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     verbose_name='ID')),
+                ('title',
+                 models.CharField(
+                     default='Название заметки',
+                     help_text='Дайте короткое название заметке',
+                     max_length=100,
+                     verbose_name='Заголовок')),
+                ('text',
+                 models.TextField(
+                     help_text='Добавьте подробностей',
+                     verbose_name='Текст')),
+                ('slug',
+                 models.SlugField(
+                     blank=True,
+                     help_text='Укажите адрес для страницы заметки. Используйте только латиницу, цифры, дефисы и знаки подчёркивания',
+                     max_length=100,
+                     unique=True,
+                     verbose_name='Адрес для страницы с заметкой')),
+                ('author',
+                 models.ForeignKey(
+                     on_delete=django.db.models.deletion.CASCADE,
+                     to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
